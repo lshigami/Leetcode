485ea61db5149663f01f5490f577596e966a08ed
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2023-07-15 00:49:49 +0700

    Time: 920 ms (5.02%), Space: 303.3 MB (5.07%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int combination(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        } else {
+            return combination(n - 1, k - 1) + combination(n - 1, k);
+        }
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>v;
+        for(int i=1;i<=n;i++){
+            v.push_back(i);
+        }
+        vector<vector<int>>vt;
+        vector<int>temp;
+        Try(vt,v,temp,combination(n,k),k,0,0);
+        return vt;
+    }
+    
+    void Try(vector<vector<int>>&vt, vector<int>v, vector<int>temp, int n, int k, int idx, int cnt){
+        for(int i=idx;i<v.size();i++){
+            temp.push_back(v[i]);
+            if(temp.size()==k){
+                vt.push_back(temp);
+                cnt++;
+                if(cnt==n) return;
+            }
+            else Try(vt,v,temp,n,k,i+1,cnt);
+            temp.pop_back();
+        }
+    }
+    
+};
