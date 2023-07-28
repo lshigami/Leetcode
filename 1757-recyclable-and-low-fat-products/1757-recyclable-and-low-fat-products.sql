bb02983e6c623976c3a6dd006228d2aa6740b5df
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2023-07-28 23:31:36 +0700

    Time: 1067 ms (35.41%), Space: 0B (100.00%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,5 @@
+-- /* Write your T-SQL query statement below */
+-- select product_id from Products
+-- where low_fats = 'Y' AND recyclable = 'Y'
+
+SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable ='Y';
