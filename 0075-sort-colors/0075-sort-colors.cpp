93954e92dbfbc3d860a80fc6bfeb199e8e6a878e
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2023-11-19 08:50:42 +0700

    Time: 0 ms (100.00%), Space: 8.5 MB (78.36%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void sortColors(vector<int>& v) {
+        int n=v.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(v[j]>v[j+1]) swap(v[j],v[j+1]);
+            }
+        }
+    }
+};
