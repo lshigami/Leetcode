cdd89ccb296c181d42ab3bad4e5ec98d33e2ea0e
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2023-11-19 08:58:06 +0700

    Time: 5 ms (5.66%), Space: 8.8 MB (15.17%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
-    void sortColors(vector<int>& v) {
-        int n=v.size();
+    //Bubble Sort
+    
+    // void sortColors(vector<int>& v) {
+    //     int n=v.size();
+    //     for(int i=0;i<n-1;i++){
+    //         for(int j=0;j<n-i-1;j++){
+    //             if(v[j]>v[j+1]) swap(v[j],v[j+1]);
+    //         }
+    //     }
+    // }
+    
+    //Selection Sort
+
+    void sortColors(vector<int>& a){
+             int n=a.size();
         for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(v[j]>v[j+1]) swap(v[j],v[j+1]);
+            int minidx=i;
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[minidx]) minidx=j;
             }
+            swap(a[i],a[minidx]);
         }
     }
 };
