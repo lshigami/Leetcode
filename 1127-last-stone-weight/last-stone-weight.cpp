d31245e6e12b6b51d05003907dd85babcf421677
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2023-11-21 18:31:08 +0700

    Time: 0 ms (100.00%) | Memory: 8 MB (30.18%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto x:stones){
+            pq.push(x);
+        }
+        while(!pq.empty()){
+            int x=pq.top();
+            pq.pop();
+            if(pq.empty()){
+                return x;
+            }
+            else{
+                int y=pq.top();
+                pq.pop();
+                if(x!=y) pq.push(x-y);   
+            }
+        }
+        return 0;
+    }
+};
