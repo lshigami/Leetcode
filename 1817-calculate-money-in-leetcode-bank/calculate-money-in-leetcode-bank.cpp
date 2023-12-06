d6d9d018aaa40436885525f3373ccac6f5ce0157
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2023-12-06 09:04:09 +0700

    Time: 3 ms (13.68%) | Memory: 6.4 MB (35.53%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+          vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+
+    int sum = 0;
+
+    int i = 1;
+
+    int stop = 0;
+
+
+
+    while (i <= n) {
+
+        sum += v[i - 1];
+
+        if (i % 7 == 0) {
+
+            int last = v.back();
+
+            v.erase(v.begin());
+
+            v.push_back(last + 1);
+
+            i = 0;
+
+        }
+
+        i++;
+
+        stop++;
+
+        if (stop == n) break;
+    }
+    return sum;
+    }
+};
