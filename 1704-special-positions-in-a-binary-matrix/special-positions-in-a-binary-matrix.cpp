b5d4becd0773fd537912bfba9d6dde5a5cb7446a
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2023-12-13 08:18:55 +0700

    Time: 19 ms (37.89%) | Memory: 13.9 MB (5.53%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& v) {
-        map<int,int>dong;
-        map<int,int>cot;
+        map<int,int>row;
+        map<int,int>col;
         for(int i=0;i<v.size();i++){
             int cnt=0;
             for(int j=0;j<v[i].size();j++){
                 if(v[i][j]==1) cnt++;
             }
-            dong[i]=cnt;
+            row[i]=cnt;
         }
         for(int j=0;j<v[0].size();j++){
             int cnt=0;
             for(int i=0;i<v.size();i++){
                 if(v[i][j]==1) cnt++;
             }
-            cot[j]=cnt;
+            col[j]=cnt;
         }
 
         int cnt=0;
 
         for(int i=0;i<v.size();i++){
             for(int j=0;j<v[i].size();j++){
-                if(v[i][j]==1 && dong[i]==1 && cot[j]==1) cnt++;
+                if(v[i][j]==1 && row[i]==1 && col[j]==1) cnt++;
             }
         }
         return cnt;
