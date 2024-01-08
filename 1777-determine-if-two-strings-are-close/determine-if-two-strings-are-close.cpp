777313078fa9b2e9acfd136007766a45a81edd21
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-01-08 17:11:26 +0700

    Time: 105 ms (61.23%) | Memory: 20.9 MB (77.11%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        unordered_map<char,int>m1;
+        unordered_map<char,int>m2;
+        vector<int>v1;
+        vector<int>v2;
+        for(auto x:word1){
+            m1[x]++;
+        }
+        for(auto x:word2){
+            m2[x]++;
+        }
+        for(auto x:m1){
+            if(m2[x.first]==0){
+                return false;
+            }
+            v1.push_back(m1[x.first]);
+            v2.push_back(m2[x.first]);
+        }
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        return word1.size()==word2.size() && v1==v2;
+    }
+};
