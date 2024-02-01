6bfab29eeddc1be4221abba91c19dcdab328da15
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-02-02 00:17:12 +0700

    Time: 8 ms (64.62%) | Memory: 13.9 MB (18.19%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -3,16 +3,16 @@ public:
     ListNode* oddEvenList(ListNode* head) {
         if (head == nullptr || head->next == nullptr)
             return head;
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;
-        while (even != nullptr && even->next != nullptr) {
-            odd->next = even->next;
-            odd = odd->next;
-            even->next = odd->next;
-            even = even->next;
+        ListNode*odd=head;
+        ListNode*even=head->next;
+        ListNode*headofeven=even;
+        while(even && even->next){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
         }
-        odd->next = evenHead;
+        odd->next=headofeven;
         return head;
     }
 };
