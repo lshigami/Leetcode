53e71bacb0cf7380de757028cd925ab3051a4671
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-02-04 12:17:32 +0700

    Time: 11 ms (62.30%) | Memory: 10.5 MB (12.49%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> m;
-        for (auto c : t)
-            m[c]++;
-
-        int count = 0;
-        int low = 0;
-        int min_length = INT_MAX, min_start = 0;
-
-        for (int high = 0; high < s.length(); high++) {
-            if (m[s[high]] > 0)
-                count++;
-            m[s[high]]--;
-            if (count == t.length()) {
-                while (low < high && m[s[low]] < 0) {
-                    m[s[low]]++;
-                    low++;
-                }
-                if (min_length > (high - low + 1)) {
-                    min_length = high - low + 1;
-                    min_start = low;
-                }
-            }
-        }
-        return (min_length == INT_MAX) ? "" : s.substr(min_start, min_length);
+       unordered_map<char,int>m;
+       for(auto x:t) m[x]++;
+       int low=0;
+       int counter=0;
+       int min_length=INT_MAX;
+       int start=0;
+       for(int i=0;i<s.size();i++){
+           if(m[s[i]]>0) counter++;
+           m[s[i]]--;
+           if(counter==t.size()){
+               while(low<i && m[s[low]]<0){
+                   m[s[low++]]++;
+               }
+               if(min_length>i-low+1){
+                   min_length=i-low+1;
+                   start=low;
+               }
+           }
+       }
+       return min_length==INT_MAX ? "": s.substr(start,min_length);
     }
 };
