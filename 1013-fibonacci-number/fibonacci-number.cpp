b13b492739d189456cdf193e42a8f64d2fbe6d41
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-02-13 13:38:52 +0700

    Time: 2 ms (77.43%) | Memory: 7.1 MB (29.88%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n - 1) + fib(n - 2);
+        int fb[35];
+        fb[0]=0;
+        fb[1]=1;
+        for(int i=2;i<35;i++) fb[i]=fb[i-1]+fb[i-2];
+        return fb[n];
     }
 };
