75a27bdf00ae77a9f855e50db228eed10f7e141c
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-02-16 22:17:22 +0700

    Time: 23 ms (46.71%) | Memory: 24.8 MB (21.06%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& a) {
-        if(a.size()<3 || a[1]<a[0]) return false;
-        bool peek=false;
-        bool isdown=false;
-        for(int i=1;i<a.size();i++){
-            if(a[i]<a[i-1]) peek=true;
-            else if(a[i]==a[i-1]) return false;
-            if(peek==true){
-                if(a[i]>a[i-1]) return false;
+        if (a.size() < 3 || a[1] < a[0])
+            return false;
+        bool peek = false;
+        for (int i = 1; i < a.size(); i++) {
+            if (a[i] < a[i - 1])
+                peek = true;
+            else if (a[i] == a[i - 1])
+                return false;
+            if (peek == true) {
+                if (a[i] > a[i - 1])
+                    return false;
             }
-            
         }
         return peek;
     }
