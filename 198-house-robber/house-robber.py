40c7fc8a4f6b153b554be800c8afbcc920805d22
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-02-17 18:52:37 +0700

    Time: 42 ms (21.93%) | Memory: 16.5 MB (60.89%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,9 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(maxsize=None)
-        def rob_recursive(i):
-            if i < 0:
-                return 0
-            return max(rob_recursive(i - 2) + nums[i], rob_recursive(i - 1))
-        
-        return rob_recursive(len(nums) - 1)
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[-1]
