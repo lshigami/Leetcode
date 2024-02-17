2980091ef568ef725300d8a42c2cace38bd8597f
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-02-17 15:13:24 +0700

    Time: 0 ms (100.00%) | Memory: 7.1 MB (26.03%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int f[38];
+        f[0]=0,f[1]=1,f[2]=1;
+        for(int i=3;i<=n;i++){
+            f[i]=f[i-3]+f[i-2]+f[i-1];
+        }
+        return f[n];
+    }
+};
