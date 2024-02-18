a3337e6b08a2b40301435a60f6af78b3285a130b
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-02-18 09:26:43 +0700

    Time: 4 ms (60.91%) | Memory: 11.6 MB (39.19%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& v, int k) {
-        sort(v.begin(),v.end());
-        for(auto x:v) cout<<x<<" ";
+        sort(v.begin(), v.end());
+        for(int i=0;i<v.size() && v[i]<0 && k>0 ;i++){
+            v[i]*=-1;
+            k--;
+        }
+        int min=INT_MAX;
+        int sum=0;
         for(int i=0;i<v.size();i++){
-            if(k==0) break;
-            if(v[i]<=0){
-                v[i]*=-1;
-                k--;
-            }
-            else{
-               if(k%2==0) return accumulate(v.begin(),v.end(),0);
-               else{
-                   sort(v.begin(),v.end());
-                   v[0]*=-1;
-                   return accumulate(v.begin(),v.end(),0);
-               }
-            }
+            min=std::min(min,v[i]);
+            sum+=v[i];
         }
-        if(k!=0){
-sort(v.begin(),v.end());
-                   v[0]*=-1;
-                
-        }
-        return accumulate(v.begin(),v.end(),0);
+        if(k%2!=0) sum-=min*2;
+        return sum;
     }
 };
