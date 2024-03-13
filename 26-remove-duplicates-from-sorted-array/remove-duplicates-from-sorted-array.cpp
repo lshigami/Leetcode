09f8cb77a1ca8e90ee26722549c46a0ba6a9addd
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-03-13 09:14:55 +0700

    Time: 10 ms (40.29%) | Memory: 21.1 MB (26.42%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,24 @@
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+
+        int slow = 0; 
+        int fast = 1; 
+        
+        while(fast<nums.size()){
+            if(nums[fast]==nums[slow]){
+                fast++;
+            }
+            else{
+                slow+=1;
+                swap(nums[fast],nums[slow]);
+                fast+=1;
+            }
+        }
+        return slow+1;
+    }
+};
