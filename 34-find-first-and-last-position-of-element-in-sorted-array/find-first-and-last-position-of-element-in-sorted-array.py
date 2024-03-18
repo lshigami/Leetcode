50d24ae6b9d32218f87dbb00c7eb48de861dfd64
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-03-18 13:38:04 +0700

    Time: 81 ms (25.13%) | Memory: 17.9 MB (48.93%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,27 +1,22 @@
 class Solution:
-    def searchRange(self, nums: List[int], x: int) -> List[int]:
-        l=0
-        r=len(nums)-1
-        a=-1
-        b=-1
-        while(l<=r):
-            m=(l+r)//2
-            if nums[m]>=x:
-                if(nums[m]==x) :
-                    a=1
-                r=m-1
-            else :
-                l=m+1
-        i=r+1
-        l=0
-        r=len(nums)-1
-        while(l<=r):
-            m=(l+r)//2
-            if nums[m]<=x:
-                if(nums[m]==x) :
-                    b=1
-                l=m+1
-            else:
-                 r=m-1
-        j=l-1
-        return [-1 if a == -1 else i, -1 if b == -1 else j]
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def BinarySearch(nums,x,isFirst):
+            l=0
+            r=len(nums)-1
+            res=-1
+            while l<=r:
+                m=(l+r)//2
+                if nums[m]>x:
+                    r=m-1
+                elif nums[m]<x:
+                    l=m+1;
+                else :
+                    res=m
+                    if isFirst:
+                        r=m-1
+                    else :
+                        l=m+1
+            return res
+        first=BinarySearch(nums,target,True)
+        last=BinarySearch(nums,target,False)
+        return [first,last]
