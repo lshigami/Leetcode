4c79c55f3713bcc28fc7312736de88aef305d375
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-03-20 19:55:32 +0700

    Time: 1469 ms (84.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,8 @@
+
+
+
+SELECT query_name, round(SUM(rating*1.0/position)*1.0/COUNT(*),2) as quality,
+                   round(count(case when rating<3 then 1 else NULL end)*100.00/count(query_name), 2) as poor_query_percentage 
+
+from queries group by query_name
+having query_name is not null
