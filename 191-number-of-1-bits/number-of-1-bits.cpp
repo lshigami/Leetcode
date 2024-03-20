4fa1ce856e9ed789be2abfe66fd7270ae2035ad7
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-03-20 22:49:36 +0700

    Time: 0 ms (100.00%) | Memory: 7.2 MB (6.41%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int counter=0;
         while(n){
-            if(n & 1) counter+=1;
-            n=n>>1;
+            counter+=1;
+            n=n&(n-1);
         }
         return counter;
     }
