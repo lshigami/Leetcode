85a97fc0799ae835b5cc4842e3ce60887bb2f633
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-03-20 23:17:06 +0700

    Time: 0 ms (100.00%) | Memory: 7 MB (68.34%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+         bool isZero ;
+        for(int i=31;i>=0;i--){
+            if(n&1){
+                ans=ans|(1<<i);
+            }else{
+                ans=ans&(~(1<<i));
+            }
+            n=n>>1;
+        }
+        return ans;
+    }
+};
