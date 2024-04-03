d594c429287d7085b15f1a277d838834bcaafac4
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-04-03 18:56:53 +0700

    Time: 881 ms (57.34%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,13 +1,20 @@
-/* Write your T-SQL query statement below */
-select (
-            case when e.name is null then s.employee_id
-                when s.salary is null then e.employee_id 
-                end
-        ) as employee_id 
-from employees e
-full join salaries s
-on s.employee_id=e.employee_id
-where name is null or salary is null
-order by case when e.name is null then s.employee_id
-                when s.salary is null then e.employee_id 
-                end
+-- /* Write your T-SQL query statement below */
+-- select (
+--             case when e.name is null then s.employee_id
+--                 when s.salary is null then e.employee_id 
+--                 end
+--         ) as employee_id 
+-- from employees e
+-- full join salaries s
+-- on s.employee_id=e.employee_id
+-- where name is null or salary is null
+-- order by case when e.name is null then s.employee_id
+--                 when s.salary is null then e.employee_id 
+--                 end
+
+select employee_id from employees where employee_id NOT IN
+(select employee_id from salaries)
+UNION
+select employee_id from salaries where employee_id NOT IN
+(select employee_id from Employees )
+order by employee_id
