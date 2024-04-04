1bf509b1649569acfc00019838b6e0d8908356e7
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-04-04 07:51:17 +0700

    Time: 5669 ms (44.06%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,9 +1,9 @@
-WITH cte AS (
-    SELECT product_id, MIN(year) AS minyear 
-    FROM sales 
-    GROUP BY product_id 
-)
-SELECT s.product_id, s.year AS first_year, s.quantity, s.price 
-FROM sales s
-INNER JOIN cte ON cte.product_id = s.product_id 
-              AND s.year = cte.minyear;
+/* Write your T-SQL query statement below */
+with cte as(
+ select product_id,MIN(year) as minyear from sales group by product_id 
+)select sales.product_id , sales.year as first_year  , sales.quantity , sales.price 
+from sales
+inner join cte
+on cte.product_id=sales.product_id and sales.year=cte.minyear
+
+
