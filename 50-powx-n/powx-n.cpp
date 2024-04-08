cb68801032e4c4b1bb139c2b2bc038ed99096dec
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-04-08 23:57:04 +0700

    Time: 4 ms (22.51%) | Memory: 8 MB (74.16%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+    double myPow(double x, long long n) {
+        if(n==0) return 1;
+        double X=myPow(x,n/2);
+        if(n>0){
+            if(n%2==0) return 1.0*X*X;
+            else return 1.0*X*X*x;
+        }else{
+            return 1.0/myPow(x,-n);
+        }
     }
 };
