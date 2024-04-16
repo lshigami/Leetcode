883fa0df4b295ff47dc55d5246700460437a07d0
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-04-16 14:07:37 +0700

    Time: 11 ms (77.20%) | Memory: 24.2 MB (74.48%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -6,31 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void DFS(TreeNode*root,int val,int i,int dep){
-        if(root==nullptr) return;
-        if(i==dep-1){
-            TreeNode*leftNode= new TreeNode(val,root->left,NULL);
-            TreeNode*rightNode= new TreeNode(val,NULL,root->right);
-            root->left=leftNode;
-            root->right=rightNode;
+    void DFS(TreeNode* root, int val, int i, int dep) {
+        if (root == nullptr)
+            return;
+        if (i <= dep - 1) {
+            if (i == dep - 1) {
+                TreeNode* leftNode = new TreeNode(val, root->left, NULL);
+                TreeNode* rightNode = new TreeNode(val, NULL, root->right);
+                root->left = leftNode;
+                root->right = rightNode;
+            }
+            DFS(root->left, val, i + 1, dep);
+            DFS(root->right, val, i + 1, dep);
         }
-        DFS(root->left,val,i+1,dep);
-        DFS(root->right,val,i+1,dep);
     }
 
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if(depth==1){
-            TreeNode*newnode= new TreeNode(val);
-            newnode->left=root;
-            root=newnode;
+        if (depth == 1) {
+            TreeNode* newnode = new TreeNode(val);
+            newnode->left = root;
+            root = newnode;
             return root;
         }
-        DFS(root,val,1,depth);
+        DFS(root, val, 1, depth);
         return root;
     }
 };
