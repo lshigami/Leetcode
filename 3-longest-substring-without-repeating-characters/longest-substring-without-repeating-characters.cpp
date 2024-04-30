4f62e2d8f94f86f456cf9bf29f8e14c84718d664
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-04-30 22:03:48 +0700

    Time: 16 ms (51.83%) | Memory: 11.2 MB (51.23%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,18 +3,21 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<char,int>m;
         int left=0;
-        int right=0;
-        int maxSize=0;
-        for(auto x:m) cout<<x.first<<" "<<x.second<<endl;
-        while(right<s.size()){
-            m[s[right]]+=1;
-            while(m[s[right]]>1){
-                m[s[left]]-=1;
-                left+=1;
+        int ans=0;
+        for(int right=0;right<s.size();right+=1){
+            if(m[s[right]]==0) {
+                m[s[right]]=1;
+                ans=max(ans,right-left+1);
             }
-            maxSize=max(maxSize,right-left+1);
-            right+=1;
+            else{
+                while(s[left]!=s[right]){
+                    m[s[left]]=0;
+                    left+=1;
+                }
+                left+=1;
+                m[s[right]]=1;
+            }
         }
-        return maxSize;
+        return ans;
     }
 };
