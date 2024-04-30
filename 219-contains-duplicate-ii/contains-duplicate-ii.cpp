474da5b934defac39d5b8333f340cbf5df94292b
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-04-30 17:21:41 +0700

    Time: 219 ms (13.80%) | Memory: 82.8 MB (18.38%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        set<int>s;
+        map<int,bool>m;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(i>k) s.erase(nums[i-k-1]);
-            if(s.find(nums[i])!=s.end()) return true;
-            s.insert(nums[i]);
+            if(i>k) m[nums[i-k-1]]=0;
+            if(m[nums[i]]) return true;
+            m[nums[i]]=1;
         }
         return false;
     }
