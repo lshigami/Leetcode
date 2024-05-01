7bd9b82d64c536a2bdfc847fa55f43b3a2d75e9c
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-05-01 14:31:27 +0700

    Time: 36 ms (78.06%) | Memory: 15.6 MB (16.56%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -4,10 +4,12 @@ public:
         if(nums.size()<3) return 0;
         sort(nums.begin(),nums.end());
         int ans = 0;
-        for(int i = 0; i < nums.size() - 2; i++){
-            for(int j = i + 1; j < nums.size() - 1; j++){
-                auto it = lower_bound(nums.begin() + j + 1, nums.end(), nums[i] + nums[j]);
-                ans += it - nums.begin() - j - 1;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int k = i + 2;
+            for (int j = i + 1; j < nums.size() - 1 && nums[i] != 0; j++) {
+                while (k < nums.size() && nums[i] + nums[j] > nums[k])
+                    k++;
+                ans += k - j - 1;
             }
         }
         return ans;
