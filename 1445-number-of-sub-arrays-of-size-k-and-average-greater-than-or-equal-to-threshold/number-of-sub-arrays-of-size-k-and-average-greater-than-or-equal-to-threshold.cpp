71e2c633f535d0e951c889eaf35f90d04b2280f6
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-05-02 16:32:07 +0700

    Time: 63 ms (48.06%) | Memory: 57.7 MB (39.00%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& v, int k, int threshold) {
-        int left=0;
-        int sum=0;
-        int ans=0;
-        threshold*=k;
-        for(int right=0;right<v.size();right++){
-            sum+=v[right];
-            if(right>=k){
-                sum-=v[left++];
+        int left = 0;
+        int sum = 0;
+        int ans = 0;
+        threshold *= k;
+        for (int right = 0; right < v.size(); right++) {
+            sum += v[right];
+            if (right >= k) {
+                sum -= v[left++];
             }
-            if(sum>=threshold && right-left+1 == k ) {
-                ans+=1;
-                cout<<sum<<" ";
-            }
+            if (sum >= threshold && right - left + 1 == k)
+                ans += 1;
         }
         return ans;
     }
