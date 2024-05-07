4ad33d4877bac28a40c2e0c9daee50cdcdf92b2f
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-05-07 12:31:22 +0700

    Time: 296 ms (27.84%) | Memory: 175.4 MB (15.25%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -1,37 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        stack<ListNode*> stack;
-        ListNode* current = head;
-
-        // Add nodes to the stack
-        while (current != nullptr) {
-            stack.push(current);
-            current = current->next;
+        stack<ListNode*>st1,st2;
+        ListNode*temp=head;
+        while(temp){
+            st1.push(temp);
+            temp=temp->next;
         }
-
-        current = stack.top();
-        stack.pop();
-        int maximum = current->val;
-        ListNode* resultList = new ListNode(maximum);
-
-        // Remove nodes from the stack and add to result
-        while (!stack.empty()) {
-            current = stack.top();
-            stack.pop();
-            // Current should not be added to the result
-            if (current->val < maximum) {
-                continue;
+        while(!st1.empty()){
+            while(!st2.empty() && st2.top()->val>st1.top()->val) {
+                st1.pop();
+                if(!st1.empty()) st1.top()->next=st2.top();
+                else return st2.top();
             }
-            // Add new node with current's value to front of the result
-            else {
-                ListNode* newNode = new ListNode(current->val);
-                newNode->next = resultList;
-                resultList = newNode;
-                maximum = current->val;
-            }
+            st2.push(st1.top());
+            st1.pop();
         }
-
-        return resultList;
+        return head;
     }
 };
