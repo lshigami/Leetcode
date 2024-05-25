dcb31c568e966b1efd4a02ac79c7dfb5d50921a9
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-05-25 11:01:52 +0700

    Time: 23 ms (98.34%) | Memory: 23.4 MB (60.36%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.cpp
@@ -0,0 +1,95 @@
+class Node{
+public:
+    int val;
+    Node*next;
+    Node(int x){
+        this->val=x;
+        this->next=nullptr;
+    }
+};
+class MyLinkedList {
+    private :
+    Node*head;
+    int size;
+public:
+    MyLinkedList() {
+        this->head=nullptr;
+        this->size=0;
+        }
+    
+    int get(int index) {
+        if(index<0 || index>=size) return -1;
+        Node*temp=head;
+        for(int i=0;i<index;i++){
+            temp=temp->next;
+        }
+        return temp->val;
+    }
+    
+    void addAtHead(int val) {
+        Node*newNode= new Node(val);
+        newNode->next=head;
+        head=newNode;
+        size+=1;
+    }
+    
+    void addAtTail(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+        size += 1;
+    }
+    
+    void addAtIndex(int index, int val) {
+        if(index==0) {
+            addAtHead(val);
+            return;
+        }
+        if(index==size) {
+            addAtTail(val);
+            return;
+        }
+        if(index>size) return;
+        Node*temp=head;
+        for(int i=0;i<index-1;i++){
+            temp=temp->next;
+        }
+        Node*newNode=new Node(val);
+        newNode->next=temp->next;
+        temp->next=newNode;
+        size+=1;
+    }
+    
+    void deleteAtIndex(int index) {
+        if(index<0 || index>=size) return;
+        if(index==0) {
+            head=head->next;
+            size-=1;
+            return;
+        }
+        Node*temp=head;
+        for(int i=0;i<index-1;i++){
+            temp=temp->next;
+        }
+        temp->next=temp->next->next;
+        size-=1;
+
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
