0c9589809e2bc36afe9cfeb07456617264cf7bca
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-07-22 19:18:43 +0700

    Time: 3 ms (66.51%) | Memory: 11 MB (20.98%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-        // 2 3 5 7 11 13 17
-        int n=deck.size();
-        sort(deck.begin(),deck.end());
+        
+        //[17]
+        //[13 17]
+        //[11 17 13]
+        //[7 13 11 17]
+        //[5 17 7 13 11 ]
+        //[3 11 5 17 7 13  ]
+        //[2 13 3 11 5 17 7 ]
         queue<int>q;
-        for(int i=0;i<n;i++){
-            q.push(i);
-        }
-        vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            ans[q.front()]=deck[i];
-            q.pop();
+        //2 3 5 7 11 13 17
+        sort(deck.begin(),deck.end(),greater<int>());
+        for(auto x:deck){
             if(!q.empty()){
                 q.push(q.front());
                 q.pop();
             }
+            q.push(x);
         }
+        vector<int>ans;
+        while(!q.empty()){
+            ans.push_back(q.front());
+            q.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
-    }
+    }  
 };
