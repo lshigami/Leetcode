1ece13ea359848a8660e59256bf0e6e8fc90a55e
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-07-22 16:54:32 +0700

    Time: 21 ms (23.22%) | Memory: 21.3 MB (20.61%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,30 +1,25 @@
 class MinStack {
-    vector<int> st; // Stack chính để lưu các giá trị
-    vector<int> ms; // Stack lưu giá trị tối thiểu tại mỗi bước
-
+    vector<int>st;
+    vector<int>ms;
 public:
     MinStack() {
+        
+    }
     
-    }
-
     void push(int val) {
+        if(ms.empty()||ms.back()>=val) ms.push_back(val);
         st.push_back(val);
-        if (ms.empty() || val <= ms.back()) {
-            ms.push_back(val);
-        } else {
-            ms.push_back(ms.back());
-        }
     }
-
+    
     void pop() {
+        if(st.back()==ms.back()) ms.pop_back();
         st.pop_back();
-        ms.pop_back();
     }
-
+    
     int top() {
         return st.back();
     }
-
+    
     int getMin() {
         return ms.back();
     }
