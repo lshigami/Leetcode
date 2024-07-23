ff4cae57cf765318225775c19eafc0a7f132b759
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-07-23 12:28:55 +0700

    Time: 13 ms (58.54%) | Memory: 12.7 MB (37.92%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int>m;
+        if(s.size()==0) return 0;
+        int ans=1;
         int left=0;
-        int ans=0;
+        unordered_map<char,int>m;
         for(int right=0;right<s.size();right+=1){
-            if(m[s[right]]==0) {
-                m[s[right]]=1;
-                ans=max(ans,right-left+1);
-            }
-            else{
-                while(s[left]!=s[right]){
-                    m[s[left]]=0;
-                    left+=1;
-                }
+            m[s[right]]+=1;
+            while(m[s[right]]>1){
+                m[s[left]]-=1;
+                if(m[s[left]]==0) m.erase(s[left]);
                 left+=1;
-                m[s[right]]=1;
             }
+            ans=max(ans,(int)m.size());
         }
         return ans;
     }
