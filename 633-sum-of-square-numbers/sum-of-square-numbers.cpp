598ba25cf069ab20121a7b3d6d98e600b92f61a6
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-07-25 23:36:06 +0700

    Time: 22 ms (13.15%) | Memory: 7.3 MB (10.31%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        long long left=1;
-        long long right=sqrt(c);
-        if((int)sqrt(c)*(int)sqrt(c)==c) return true;
-        while(left<=right){
-            long long product=left*left+ right*right;
-            if(product>c) right-=1;
-            else if (product<c) left+=1;
-            else return true;
+        //b=sqrt(c-a*a);
+        for(int a=0;a<=sqrt(c);a+=1){
+            double b=1.0*sqrt(c-a*a);
+            if(fmod(b,1)==0) return true;
         }
         return false;
     }
