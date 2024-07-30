1e0adf56140456bd450c7a920e0e776783640cd7
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-07-30 18:27:30 +0700

    Time: 8 ms (6.27%) | Memory: 12.2 MB (5.61%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,21 +1,28 @@
 class Solution {
+private:
+    unordered_map<int, bool> m;
+
 public:
-    unordered_map<int,bool>m;
-    void generate(vector<vector<int>>&ans,vector<int>temp,vector<int>& nums){
-        for(int i=0;i<nums.size();i++){
-            if(m[nums[i]]) continue;
-            m[nums[i]]=1;
-            temp.push_back(nums[i]);
-            if(temp.size()==nums.size()) ans.push_back(temp);
-            else generate(ans,temp,nums);
-            temp.pop_back();
-            m[nums[i]]=0;
-        }
-    }
     vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>cur;
         vector<vector<int>>ans;
-        vector<int>temp;
-        generate(ans,temp,nums);
+        backtracking(nums,cur,ans);
         return ans;
+    }
+    void backtracking(vector<int> nums, vector<int> cur,vector<vector<int>>& ans) {
+        for(int i=0;i<nums.size();i++){
+            if(m[nums[i]]==0){
+                cur.push_back(nums[i]);
+                m[nums[i]]=1;
+                if(cur.size()==nums.size()) {
+                    ans.push_back(cur);
+                }
+                else{
+                    backtracking(nums,cur,ans);
+                }
+                m[nums[i]]=0;
+                cur.pop_back();
+            }
+        }
     }
 };
