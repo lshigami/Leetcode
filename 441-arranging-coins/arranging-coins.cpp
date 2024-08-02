cf3373111c659f4f4802e1740de19fbd386f94f3
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-02 23:46:19 +0700

    Time: 6 ms (43.69%) | Memory: 8.7 MB (6.15%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int left=1;
-        int right=n;
-        int ans=n;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            long long value=1ll*mid*(mid+1)/2;
-            if(value<=n){
-                ans=mid;
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
+        long long left = 1;
+        long long right = (-1 + sqrt(1 + 8 * (long long)n)) / 2;
+        int ans = 0;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long value = mid * (mid + 1) / 2;
+            if (value <= n) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return ans;
