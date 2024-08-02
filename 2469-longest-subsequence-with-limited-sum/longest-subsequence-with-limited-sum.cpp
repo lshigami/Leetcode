7257cbea66ad0102165fc06f6a255a4e94079b80
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-02 16:42:05 +0700

    Time: 8 ms (92.32%) | Memory: 16.7 MB (17.77%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -3,16 +3,17 @@ public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(),nums.end());
         vector<int>ans;
+        vector<int>prefix(nums.size());
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        //1 2 4 5 
+        //1 3 7 12
         for(auto x:queries){
-            int sum=0;
-            int MAX=0;
-            for(int i=0;i<nums.size();i++){
-                sum+=nums[i];
-                if(sum<=x){
-                    MAX=max(MAX,i+1);
-                }
-            }
-            ans.push_back(MAX);
+            auto it=lower_bound(prefix.begin(),prefix.end(),x);
+            if(it==prefix.end() || *it!=x ) ans.push_back(it-prefix.begin());
+            else ans.push_back(it-prefix.begin()+1);
         }
         return ans;
     }
