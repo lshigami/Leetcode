563c23a3556a1bd92d20b2353c2bee1ee78cd1ff
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-02 10:21:19 +0700

    Time: 11 ms (12.13%) | Memory: 16.3 MB (18.20%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         auto it = lower_bound(nums.begin(), nums.end(), target);
         if (it == nums.end() || *it != target) return v;
         v[0] = it - nums.begin();
-        auto eit = upper_bound(nums.begin(), nums.end(), target);
+        auto eit = upper_bound(it, nums.end(), target);
         v[1] = eit - nums.begin() - 1;
         return v;
     }
