83c41aee262e198aca9137a4c6e79b97a196d4e3
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-02 14:28:21 +0700

    Time: 2 ms (51.38%) | Memory: 8.3 MB (6.18%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int left=0;
+        int right=x;
+        int ans=x;
+        while(left<=right){
+            long long mid=(left+right)/2;
+            if(mid*mid>x){
+                ans=mid;
+                right=mid-1;
+            }
+            else if(mid*mid==x) return mid;
+            else left=mid+1;
+        }
+        return right;
     }
 };
