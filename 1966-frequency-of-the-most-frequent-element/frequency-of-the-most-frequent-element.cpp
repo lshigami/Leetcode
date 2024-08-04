a751346263c3005337676940394b380c39479e39
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-04 12:00:39 +0700

    Time: 199 ms (9.16%) | Memory: 316.8 MB (5.30%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    bool valid(int size, const std::vector<long long>& prefix, int k, const std::vector<int>& nums) {
+    //1 5 13 26
+    bool valid(int size, vector<long long> prefix, int k, vector<int> nums) {
         for (int i = size - 1; i < prefix.size(); ++i) {
-            long long sum = prefix[i];
-            if (i >= size) {
-                sum -= prefix[i - size];
-            }
-
-            // Calculate the required number of operations to make all elements in the window equal to nums[i]
+            long long sum = prefix[i]-((i>=size ? prefix[i-size] : 0));
             long long needed = (long long) nums[i] * size - sum;
             if (needed <= k) {
                 return true;
@@ -17,18 +13,16 @@ public:
     }
 
     int maxFrequency(std::vector<int>& nums, int k) {
-        std::sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        std::vector<long long> prefix(n);
+        vector<long long> prefix(n);
         prefix[0] = nums[0];
         for (int i = 1; i < n; ++i) {
             prefix[i] = prefix[i - 1] + nums[i];
         }
-
         int left = 1;
         int right = n;
         int result = 1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (valid(mid, prefix, k, nums)) {
@@ -38,7 +32,6 @@ public:
                 right = mid - 1;
             }
         }
-
         return result;
     }
 };
