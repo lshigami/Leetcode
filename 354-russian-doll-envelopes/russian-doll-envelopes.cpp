bbe21d80aa91e1d2536c187ed0014febf897f155
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-05 22:23:33 +0700

    Time: 442 ms (12.31%) | Memory: 102 MB (13.62%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -3,15 +3,17 @@ public:
     int lowerBound(const vector<vector<int>>& envs, const vector<int>& value) {
         int left = 0;
         int right = envs.size() - 1;
+        int ans=envs.size();
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (envs[mid][1] >= value[1]) {
+                ans=mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 
     int maxEnvelopes(vector<vector<int>>& envelopes) {
@@ -20,6 +22,9 @@ public:
         });
 
         vector<vector<int>> res;
+        for(auto x:envelopes){
+            cout<<x[0]<<" "<<x[1]<<endl;
+        }
         for (const auto& env : envelopes) {
             int index = lowerBound(res, env);
             if (index == res.size()) {
