6146967374531477b915afbd2d673fe7e0bcf40a
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-05 20:23:58 +0700

    Time: 10 ms (83.40%) | Memory: 36 MB (41.99%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
+    int lowerBound(vector<int>nums,int value){
+        int left=0;
+        int right=nums.size()-1;
+        int ans=nums.size();
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=value){
+                ans=mid;
+                right=mid-1;
+            }
+            else {
+                left=mid+1;
+            }
+        }
+        return ans;
+
+    }
     int lengthOfLIS(vector<int>& nums) {
         vector<int>ans;
         ans.push_back(nums[0]);
         for(int i=1;i<nums.size();i++){
-            auto it=lower_bound(ans.begin(),ans.end(),nums[i]);
-            if(it==ans.end()){
+            int index=lowerBound(ans,nums[i]);
+            if(index==ans.size()){
                 ans.push_back(nums[i]);
             }
             else{
-                int index=it-ans.begin();
                 ans[index]=nums[i];
             }
         }
