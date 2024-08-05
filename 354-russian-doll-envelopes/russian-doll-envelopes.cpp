c61b3f53494d8b8a9eeb873399f18491f06eb8b2
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-05 21:58:22 +0700

    Time: 217 ms (31.56%) | Memory: 91.6 MB (19.98%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int lowerBound(const vector<vector<int>>& envs, vector<int> value) {
-        int index = envs.size();
+    int lowerBound(const vector<vector<int>>& envs, const vector<int>& value) {
         int left = 0;
         int right = envs.size() - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (envs[mid][1] >= value[1]) {
-                index = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return index;
+        return left;
     }
-    
+
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        // Sắp xếp theo chiều rộng tăng dần và chiều cao giảm dần nếu chiều rộng bằng nhau
-        sort(envelopes.begin(), envelopes.end(), [](vector<int> a, vector<int> b) {
-            if (a[0] == b[0]) return a[1] > b[1];
-            return a[0] < b[0];
+        sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b) {
+            return (a[0] < b[0]) || (a[0] == b[0] && a[1] > b[1]);
         });
-        
+
         vector<vector<int>> res;
-        res.push_back(envelopes[0]);
-        for (int i = 1; i < envelopes.size(); i++) {
-            int index = lowerBound(res, envelopes[i]);
+        for (const auto& env : envelopes) {
+            int index = lowerBound(res, env);
             if (index == res.size()) {
-                res.push_back(envelopes[i]);
+                res.push_back(env);
             } else {
-                res[index] = envelopes[i];
+                res[index] = env;
             }
         }
         return res.size();
