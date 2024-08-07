67f0006e5cee5dd2a07ae23578952d36546d4a54
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-07 16:10:41 +0700

    Time: 130 ms (54.85%) | Memory: 120.6 MB (46.02%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& A, int k) {
-        int a = *max_element(A.begin(), A.end()), n = A.size(), cur = 0, i = 0;
-        long long res = 0;
-        for (int j = 0; j < n; ++j) {
-            cur += A[j] == a;
-            while (cur >= k)
-                cur -= A[i++] == a;
-            res += i;
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maximum = *max_element(nums.begin(),nums.end());
+        long long ans=0;
+        int left=0;
+        int counter=0;
+        //[1,3,2,3,1]
+        // 0 1 2 3 4
+        for(int right=0;right<nums.size();right+=1){
+            if(nums[right]==maximum) counter+=1;
+            while(counter==k){
+                ans+=nums.size()-right;
+                if(nums[left]==maximum) counter-=1;
+                left+=1;
+            }
         }
-        return res;
+
+        return ans;
     }
 };
