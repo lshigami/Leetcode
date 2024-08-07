1cb3a106113aab8d0c1b3f17117e0eed291c9c55
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-07 14:18:04 +0700

    Time: 388 ms (5.00%) | Memory: 176.5 MB (5.16%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
+    // B A A A B C  : k=2
+    bool valid(unordered_map<char,int>m,int k){
+        bool isOk=true;
+        int maxFrequency=INT_MIN;
+        int charMax;
+        for(auto x:m){
+            if(x.second>maxFrequency){
+                charMax=x.first;
+                maxFrequency=x.second;
+            }
+        }
+        for(auto x:m){
+            if(k<0) return false;
+            if(x.second==maxFrequency && isOk){
+                isOk=false;
+                continue;
+            }
+            else{
+                k-=x.second;
+            }
+        }
+        return k>=0;
+    }
     int characterReplacement(string s, int k) {
-        map<char,int>m;
         int left=0;
-        int ans=1;
+        int maxSize=1;
+        unordered_map<char,int>m;
         for(int right=0;right<s.size();right+=1){
             m[s[right]]+=1;
-            while(!canBeReplace(m,left,right,k)){
-                m[s[left++]]-=1;
+            while(m.size()>1 && !valid(m,k)){
+                if(m[s[left]]==1) m.erase(s[left]);
+                else m[s[left]]-=1;
+                left+=1;
             }
-            ans=max(ans,right-left+1);
+            maxSize=max(maxSize,right-left+1);
         }
-        return ans;
-    }
-    bool canBeReplace(map<char,int>m,int l,int r,int k){
-        int MAX=0;
-        for(auto x:m){
-            MAX=max(MAX,x.second);
-        }
-        return r-l+1-MAX<=k;
+        return maxSize;
     }
 };
