1f36c76f641c8586237fda60d1542668e182ab89
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-13 15:42:16 +0700

    Time: 38 ms (67.66%) | Memory: 59 MB (5.17%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool possible(int maxWeight,vector<int>weights,int days){
+        int currentWeight=0;
+        int day=1;
+        for(auto w:weights){
+            currentWeight+=w;
+            if(currentWeight>maxWeight){
+                currentWeight=w;
+                if(currentWeight>maxWeight) return false;
+                day+=1;
+            }
+        }
+        return day<=days;
+    }
+    //1 2 2 3 4 4
+    int shipWithinDays(vector<int>& weights, int days) {
+        // sort(weights.begin(),weights.end());
+        int left=weights[weights.size()-1];
+        int right=accumulate(weights.begin(),weights.end(),0);
+        int minWeight=right;
+        while(left<=right){ 
+            int mid=left+(right-left)/2;
+            if(possible(mid,weights,days)){
+                minWeight=mid;
+                right=mid-1;
+            }
+            else {
+                left=mid+1;
+            }
+        }
+        return minWeight;
+    }
+};
