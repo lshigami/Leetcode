07e86403c93ea19ba3f07dbc4bbabb5de594ff3a
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-14 10:55:18 +0700

    Time: 130 ms (25.43%) | Memory: 124.8 MB (5.85%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    bool valid(const std::vector<int>& bloomDay, int mid, int m, int k) {
-        int bouquets = 0, flowers = 0;
-        for (int day : bloomDay) {
-            if (day <= mid) {
-                flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
-                }
-            } else {
-                flowers = 0;
+    bool valid(int mid,vector<int>bloomDay,int m,int k){
+        int bouquets=0;
+        int left=0;
+        for(int right=0;right<bloomDay.size();right++){
+            if(bloomDay[right]>mid) {
+                left=right+1;
+                continue;
             }
-            if (bouquets >= m) return true;
+            if(right-left+1==k) {
+                left=right+1;
+                bouquets+=1;
+                continue;
+            }
         }
-        return bouquets >= m;
+        cout<<bouquets;
+        return bouquets>=m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        if(1ll*m*k>bloomDay.size()) return -1;
         int left=*min_element(bloomDay.begin(),bloomDay.end());
         int right=*max_element(bloomDay.begin(),bloomDay.end());
         int ans=-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(valid(bloomDay,mid,m,k)){
-                ans=mid;
+            if(valid(mid,bloomDay,m,k)){
                 right=mid-1;
+                ans=mid;
             }
             else{
                 left=mid+1;
