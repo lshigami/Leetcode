ed0037ead9fe788838d0dcb83fe1e3cbfa0aed2e
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-14 23:17:26 +0700

    Time: 0 ms (100.00%) | Memory: 11.3 MB (40.02%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
-        int k=m+n-1;
-        int i=m-1;
-        int j=n-1;
-        while(i>=0 && j>=0){
-            if(v2[j]>v1[i]) v1[k--]=v2[j--];
-            else v1[k--]=v1[i--];
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int end=m+n-1;
+        int i=m-1,j=n-1;
+        while(i>=0 && j>=0 && end>=0){
+            if(nums2[j]>nums1[i]){
+                nums1[end--]=nums2[j--];
+            }
+            else{
+                nums1[end--]=nums1[i--];
+            }
         }
-        while(j>=0){
-            v1[k--]=v2[j--];
+        while(j>=0 && end>=0){
+            nums1[end--]=nums2[j--];
         }
     }
 };
