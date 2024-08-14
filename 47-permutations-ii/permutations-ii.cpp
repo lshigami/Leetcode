eee5ca91baed47455ff2b7497feffff3daeb7cf6
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-14 20:27:11 +0700

    Time: 8 ms (50.18%) | Memory: 11.2 MB (68.84%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -10,26 +10,15 @@ public:
     }
 
     void backtrack(vector<int>& nums, vector<int>& cur, vector<vector<int>>& ans, vector<bool>& used) {
-        if (cur.size() == nums.size()) {
-            ans.push_back(cur);
-            return;
-        }
-        
         for (int i = 0; i < nums.size(); i++) {
-            // Nếu phần tử đã được sử dụng, bỏ qua.
             if (used[i]) continue;
-
-            // Bỏ qua các phần tử trùng lặp nếu phần tử trước đó giống hệt và chưa được sử dụng.
             if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-
-            // Đánh dấu phần tử hiện tại là đã sử dụng.
             used[i] = true;
             cur.push_back(nums[i]);
-
-            // Gọi đệ quy với trạng thái mới.
-            backtrack(nums, cur, ans, used);
-
-            // Hoàn tác thay đổi.
+            if (cur.size() == nums.size()) {
+                ans.push_back(cur);
+            }
+            else backtrack(nums, cur, ans, used);
             used[i] = false;
             cur.pop_back();
         }
