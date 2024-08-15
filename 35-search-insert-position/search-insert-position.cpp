66aa9c9e343d409d6cfc0d95cbd0bbed697cf8cb
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-15 09:37:47 +0700

    Time: 7 ms (23.49%) | Memory: 12.4 MB (36.69%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& v, int target) {
-        int l=0;
-        int r=v.size()-1;
-        while(l<=r){
-            int m=(l+r)/2;
-            if(v[m]==target){
-                return m;
-            }
-            else if(v[m]>target){
-                r=m-1;
-            }
-            else {
-                l=m+1;
-            }
-            
-        }
-       return l;
+    int searchInsert(vector<int>& nums, int target) {
+        auto it=lower_bound(nums.begin(),nums.end(),target);
+        // if(it==nums.end()) return nums.size();
+        return it-nums.begin();
     }
 };
