8af6d76172e5bee07b30f06b298d83c7567f5c54
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-16 14:09:58 +0700

    Time: 97 ms (14.20%) | Memory: 96 MB (63.22%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        stack<int>st;
-        vector<int>right(prices.size(),-1);
-        for(int i=prices.size()-1;i>=0;i--){
-            while(!st.empty() && prices[i]>=prices[st.top()]) st.pop();
-            if(!st.empty()) right[i]=st.top();
-            st.push(i);
-        }
+        int MIN=prices[0];
+        int MAX=prices[0];
         int profit=0;
-        for(auto x:right) cout<<x<<" ";
-        for(int i=0;i<prices.size();i++){
-            if (right[i] != -1) {
-                int indexGreater=right[i];
-                while(right[indexGreater]!=-1){
-                    right[i]=right[indexGreater];
-                    indexGreater=right[indexGreater];
-                }
-                profit=max(profit,prices[right[i]] - prices[i]);
-            }
+        for(int i=1;i<prices.size();i++){
+            MIN=min(MIN,prices[i]);
+            profit=max(profit,prices[i]-MIN);
         }
         return profit;
     }
