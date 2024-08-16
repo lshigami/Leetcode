1262fafd44a197901c189793532cff74a54d6c6b
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-16 09:52:40 +0700

    Time: 111 ms (17.62%) | Memory: 64.7 MB (8.19%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        vector<int>left,mid,right;
-        int pivot = nums[rand()%nums.size()];
+        priority_queue<int> pq;
         for(auto x:nums){
-            if(x>pivot) right.push_back(x);
-            else if(x==pivot) mid.push_back(x);
-            else left.push_back(x);
+            pq.push(x);
         }
-        if(k<=right.size()) return findKthLargest(right,k);
-        k-=right.size();
-        if(k<=mid.size()) return pivot;
-        k-=mid.size();
-        return findKthLargest(left,k);
+        for(int i=1;i<k;i++){
+            pq.pop();
+        }
+        return pq.top();
     }
 };
