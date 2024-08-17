ff82de0f5ed54f9fe1efdd4abf125316d9f5fca9
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-17 16:43:10 +0700

    Time: 3 ms (64.89%) | Memory: 11.6 MB (7.82%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -2,13 +2,22 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int l = 0, r = nums.size() - 1;
-        while (l < r) {
+        int result = -1; // Initialize result to store the peak index
+        
+        while (l <= r) {
             int mid = (l + r) / 2;
-            if (nums[mid] > nums[mid + 1])
-                r = mid;
-            else
-                l = mid + 1;
+            if (mid == 0 || nums[mid] > nums[mid - 1]) {  // Edge case when mid is at the start
+                if (mid == nums.size() - 1 || nums[mid] > nums[mid + 1]) {  // Edge case when mid is at the end
+                    result = mid;  // Found the peak
+                    break;
+                } else {
+                    l = mid + 1;  // Move right
+                }
+            } else {
+                r = mid - 1;  // Move left
+            }
         }
-        return l;
+        
+        return result;
     }
 };
