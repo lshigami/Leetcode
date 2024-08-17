34b2626d98bd74d95abf639199c5f6c2881640c6
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-17 16:34:29 +0700

    Time: 3 ms (64.89%) | Memory: 11.6 MB (40.71%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& v) {
-        if (v.size() == 1)
-            return 0;
-
-        if (v[0] > v[1])
-            return 0;
-        if (v[v.size() - 1] > v[v.size() - 1 - 1])
-            return v.size() - 1;
-        int l = 0;
-        int r = v.size() - 1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (m + 1 < v.size() && m - 1 >= 0 && v[m] > v[m + 1] &&
-                v[m] > v[m - 1])
-                return m;
-            else if (m - 1 >= 0 && v[m] < v[m - 1])
-                r = m - 1;
-            else if (m + 1 < v.size() && v[m] < v[m + 1])
-                l = m + 1;
-            else
-                l = m + 1;
+    int findPeakElement(vector<int>& nums) {
+        if(nums.size()==1) return 0;
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(mid==0){
+                if(nums[mid+1]<nums[mid]) return mid;
+                else left=mid+1;
+            }
+            else if(mid==nums.size()-1){
+                if(nums[mid-1]<nums[mid]) return mid;
+                else right=mid-1;
+            }
+            else{
+                if(nums[mid-1]<=nums[mid] && nums[mid]>=nums[mid+1]) return mid;
+                else if(nums[mid-1]>=nums[mid] || nums[mid]>=nums[mid+1]) right=mid-1;
+                else if(nums[mid-1]<=nums[mid] || nums[mid]<=nums[mid+1]) left=mid+1;
+            }
         }
         return -1;
     }
