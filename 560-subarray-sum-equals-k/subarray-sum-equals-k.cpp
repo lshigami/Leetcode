08636adc4bc47f179ad8d1931b6899f1fb0fc659
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-08-21 22:10:31 +0700

    Time: 63 ms (75.92%) | Memory: 46.4 MB (42.94%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> preSum;
-        preSum[0] = 1;
-        int sum = 0, ans = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            if (preSum.find(sum - k) != preSum.end()) {
-                ans += preSum[sum - k];
-            }
-            preSum[sum]++;
+        vector<int>prefix(nums.size());
+        unordered_map<int,int>m;
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            prefix[i]=prefix[i-1]+nums[i];
+            
         }
-        return ans;
+        int cnt=0;
+        for(int i=0;i<nums.size();i+=1){
+            if(m.find(prefix[i]-k)!=m.end()) cnt+=m[prefix[i]-k];
+            m[prefix[i]]+=1;
+        }
+        cnt+=m[k];
+        return cnt;
+        //1 2 3
+        //1 3 6
+        //pre[j]-pre[i]=k
+        //-1 -2 -1
+
+        //1 0 0
+        
+
     }
 };
