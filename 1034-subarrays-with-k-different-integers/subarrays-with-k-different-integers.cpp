2c353534d2a0208e8c4a94d9914d0fa5ea4aa525
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-09-01 11:02:58 +0700

    Time: 125 ms (62.13%) | Memory: 56.3 MB (39.92%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,40 +1,21 @@
-int atMostKDistinct(vector<int>& A,int n,int k){
-    int count = 0;
-    int left = 0;
-    int right = 0;
-    
-    unordered_map<int,int> mp;
-    
-    while(right < n){
-        
-        //add current element in the map
-        if(mp.find(A[right]) == mp.end()){
-            mp[A[right]] = 1;
-        }else{
-            mp[A[right]]++;
-        }
-        
-        while(mp.size() > k){
-            mp[A[left]]--;
-            if(mp[A[left]] == 0){
-                mp.erase(A[left]);
-            }
-            left++;
-        }
-        
-        count += right-left+1;
-        right++;
-    }
-    
-    
-    return count;
-}
-
-
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& A, int k) {
-        int n = A.size();
-        return atMostKDistinct(A,n,k) - atMostKDistinct(A,n,k-1);
+    int subarrayWithAtMostKDistinct(vector<int>&nums,int k){
+        unordered_map<int,int>m;
+        int res=0;
+        int left=0;
+        for(int right=0;right<nums.size();right+=1){
+            m[nums[right]]+=1;
+            while(m.size()>k){
+                if(m[nums[left]]==1) m.erase(nums[left]);
+                else m[nums[left]]-=1;
+                left+=1;
+            }
+            res+=(right-left+1);
+        }
+        return res;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return subarrayWithAtMostKDistinct(nums,k)-subarrayWithAtMostKDistinct(nums,k-1);
     }
 };
