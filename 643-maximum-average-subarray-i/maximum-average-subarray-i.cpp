285fbf33ca7e11f87876aefc203a14fb9ffe03ab
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-09-02 10:30:28 +0700

    Time: 131 ms (33.63%) | Memory: 112.7 MB (67.03%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
+        int left=0;
+        double avg=INT_MIN;
+        int sum=0;
+        for(int right=0;right<nums.size();right+=1){
+            if(right>=k){
+                sum-=nums[left++];
+            }
+            sum+=nums[right];
+            if(right-left+1==k) avg=max(avg,1.0*sum/k);
         }
-        double ans = sum;
-        for (int i = k; i < nums.size(); i++) {
-            sum += nums[i] - nums[i - k];
-            ans = max(ans, sum);
-        }
-        return ans / k;
+        return avg;
     }
 };
