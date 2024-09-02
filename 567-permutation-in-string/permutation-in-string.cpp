30ff4dd23ce1f4b89875ac9b512099f77f064ad6
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-09-02 08:14:46 +0700

    Time: 12 ms (35.68%) | Memory: 10.7 MB (27.60%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    bool checkInclusion(string p, string s) {
-        if(p.size()>s.size()) return false;
-        map<char,int>m;
-        for(auto x:p) m[x]+=1;
-        int k=p.size(); // size of window
-        vector<int>ans;
-        map<char,int>temp;
-        int left=0;
-        for(int i=0;i<k;i++){
-            temp[s[i]]+=1;
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int>m1,m2;
+        for(auto x:s1){
+            m1[x]+=1;
         }
-        if(m==temp) return true;
-        for(int i=k;i<s.size();i++){
-           temp[s[i]]+=1;
-           if(temp[s[left]]==1) temp.erase(s[left++]);
-           else temp[s[left++]]-=1;
-           if(temp==m) return true;
+        int left=0;
+        int k=s1.size();
+        for(int i=0;i<min(k,(int)s2.size());i++){
+            m2[s2[i]]+=1;
+        }
+        if(m1==m2) return true;
+        for(int i=k;i<s2.size();i++){
+            m2[s2[i]]+=1;
+            if(m2[s2[left]]==1) m2.erase(s2[left++]);
+            else m2[s2[left++]]-=1;
+            if(m1==m2) return true;
         }
         return false;
     }
