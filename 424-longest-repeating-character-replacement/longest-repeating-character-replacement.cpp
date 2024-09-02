aa6821efef191e652a6c11ae4b103e40c9712084
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2024-09-02 10:05:29 +0700

    Time: 32 ms (9.33%) | Memory: 10.6 MB (5.45%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    // B A A A B C  : k=2
-    bool valid(unordered_map<char,int>m,int k){
-        bool isOk=true;
-        int maxFrequency=INT_MIN;
-        int charMax;
+    bool valid(map<char,int>&m,int k){
+        int maxChar;
+        int maxFre=INT_MIN;
         for(auto x:m){
-            if(x.second>maxFrequency){
-                charMax=x.first;
-                maxFrequency=x.second;
+            if(x.second>maxFre){
+                maxFre=x.second;
+                maxChar=x.first;
             }
         }
         for(auto x:m){
+            if(x.first==maxChar) continue;
+            else k-=x.second;
             if(k<0) return false;
-            if(x.second==maxFrequency && isOk){
-                isOk=false;
-                continue;
-            }
-            else{
-                k-=x.second;
-            }
         }
-        return k>=0;
+        return true;
     }
     int characterReplacement(string s, int k) {
         int left=0;
-        int maxSize=1;
-        unordered_map<char,int>m;
+        int MAX=1;
+        map<char,int>m;
         for(int right=0;right<s.size();right+=1){
             m[s[right]]+=1;
-            while(m.size()>1 && !valid(m,k)){
-                if(m[s[left]]==1) m.erase(s[left]);
-                else m[s[left]]-=1;
-                left+=1;
+            if(m.size()>1){
+                while(!valid(m,k)){
+                    if(m[s[left]]==1) m.erase(s[left]);
+                    else m[s[left]]-=1;
+                    left+=1;
+                }
             }
-            maxSize=max(maxSize,right-left+1);
+            MAX=max(MAX,right-left+1);
         }
-        return maxSize;
+        return MAX;
     }
 };
