c5eb39851d63e0919622f9b4bb8e4daf90085f3b
Nguyễn Quang Thắng <135859127+lshigami@users.noreply.github.com>
2025-02-23 14:24:55 +0700

    Time: 4 ms (2.97%) | Memory: 42.2 MB (39.11%) - LeetSync

--- 1404-print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.java
+++ 1404-print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.java
@@ -2,21 +2,22 @@
  * // This is the ImmutableListNode's API interface.
  * // You should not implement it, or speculate about its implementation.
  * interface ImmutableListNode {
- * public void printValue(); // print the value of this node.
- * public ImmutableListNode getNext(); // return the next node.
+ *     public void printValue(); // print the value of this node.
+ *     public ImmutableListNode getNext(); // return the next node.
  * };
  */
 
 class Solution {
     public void printLinkedListInReverse(ImmutableListNode head) {
-        List<ImmutableListNode> arr = new ArrayList<>();
-        ImmutableListNode current = head;
-        while (current.getNext() != null) {
-            arr.add(current);
-            current = current.getNext();
+        ImmutableListNode run;
+        ImmutableListNode end=null;
+        while(head!=end){
+            run=head;
+            while(run.getNext()!=end){
+                run=run.getNext();
+            }
+            run.printValue();
+            end=run;
         }
-        arr.add(current);
-        Collections.reverse(arr);
-        arr.forEach(n -> n.printValue());
     }
 }
